7ec0d43a0a16a2fb88b1aeb7a0c3e056430b93e1
terrybjr <bkterryjr@gmail.com>
2024-04-13 20:22:05 -0500

    added logging

--- btd-app-ejb/src/main/java/com/brianthedeveloper/helloworld/HelloWorldSLS.java
+++ btd-app-ejb/src/main/java/com/brianthedeveloper/helloworld/HelloWorldSLS.java
@@ -1,11 +1,16 @@
 package com.brianthedeveloper.helloworld;
 
 import jakarta.ejb.Stateless;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Stateless
 public class HelloWorldSLS {
+    private static final Logger logger = LogManager.getLogger("HelloWorldSLS");
 
     public String sayHello() {
-        return "Hello World2!";
+        logger.debug("Hello World4!");
+
+        return "Hello World4!";
     }
 }
--- btd-app-ws/src/main/java/com/brianthedeveloper/rs/HelloWorld.java
+++ btd-app-ws/src/main/java/com/brianthedeveloper/rs/HelloWorld.java
@@ -6,14 +6,25 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 @Path("/example")
 public class HelloWorld {
     @EJB
     HelloWorldSLS helloWorldSLS;
+
     @GET
     @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
+    @APIResponses(
+            value = {
+                    @APIResponse(
+                            responseCode = "200",
+                            description = "Successful, returning hello world"
+                    )
+            }
+    )
     public String getHello() {
         return helloWorldSLS.sayHello();
     }
